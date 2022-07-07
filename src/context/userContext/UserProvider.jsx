942fed27a6efd0c1d96ac054c3bdeee1285472f5
Manuel <antdev1987@gmail.com>
2022-07-06 22:57:03 -0600

    test 3

--- src/context/userContext/UserProvider.jsx
+++ src/context/userContext/UserProvider.jsx
@@ -31,7 +31,7 @@ export const UserProvider = props => {
             //const endPoint = `http://192.168.100.248:4000/api/user/login`
             //const endPoint = `https://actas-server.herokuapp.com/api/user/login`
 
-            const endPoint = `${import.meta.env.VITE.URL}/user/login`
+            const endPoint = `${import.meta.env.VITE_URL}/user/login`
 
             const { data } = await axios.post(endPoint, userData)
             // Swal.fire({
